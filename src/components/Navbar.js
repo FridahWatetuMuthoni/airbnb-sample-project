b453307355b634b5ae2f9938bcd1467258f4a303
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-01-12 17:02:26 +0300

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,18 @@
 import logo from '../images/air-logo.png';
 
+
 const Navbar = () => {
     return (
         <nav className="nav">
             <section className='navbar'>
                 <div className="logo">
                     <img src={ logo} alt="" />
+                </div>
+                <div>
+                    <input type="checkbox" name="checkbox" id="checkbox" />
+                    <label htmlFor="checkbox" className='label'>
+                        <div className="ball"></div>
+                    </label>
                 </div>
         </section>
         </nav>
